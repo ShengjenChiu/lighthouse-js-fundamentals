ad68f3bc8a0641e4ae434f7f3839481e28107134
Shengjen Chiu <sheng.chiu@shaw.ca>
2022-04-09 16:59:40 -0600

    loop over an array

--- packing-list.js
+++ packing-list.js
@@ -1,9 +1,18 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+// //Iterating Over an Array With for loop that starts from 0.
+// console.log("Kitchen stuff to pack:");
+// for (let i = 0; i < packingList.length; i++) {
+//   console.log(packingList[i]);
+// }
+
+// packingList[3];
+
+
+//Iterating Over an Array With while loop
 console.log("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
+let i = 0;
+while (i < packingList.length) {
   console.log(packingList[i]);
+  i++;
 }
-
-packingList[3];
-
